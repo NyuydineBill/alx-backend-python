6db5b12a10b3bd26fc460982d5ee620c53da5407
NyuydineBill <billleynyuy@gmail.com>
2024-08-03 01:08:45 +0100

    done with first task

---
+++ 0x00-python_variable_annotations/102-main.py
@@ -0,0 +1,7 @@
+Success: no issues found in 1 source file
+bob@dylan:~$ cat 102-main.py 
+#!/usr/bin/env python3
+
+zoom_array =  __import__('102-type_checking').zoom_array
+
+print(zoom_array.__annotations__)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+Python is a dynamically-typed language. That means that variable types are dynamically set at run-time, upon assignment of a value to a variable.
